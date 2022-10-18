2a7358310c124075f3056bcb7fb0860c503230aa
Yu hee chang <yhc10215@gmail.com>
2022-10-18 17:05:45 +0900

    환경 세팅

---
+++ src/test/java/com/example/goldenage/GoldenAgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goldenage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenAgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
