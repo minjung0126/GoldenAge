b2f89aac20703c28ea64a07a3e46e13c9802b166
intectme12 <intectme12@gmail.com>
2022-10-23 14:42:00 +0900

    jihwan

--- src/main/java/com/example/goldenage/config/SpringSecurityConfig.java
+++ src/main/java/com/example/goldenage/config/SpringSecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.goldenage.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
