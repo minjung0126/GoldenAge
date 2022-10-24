5691704567237c560fb364fb272f0f53a9699131
Yu heechang <yhc102155@gmail.com>
2022-10-24 23:42:20 +0900

    타임리프가 나타났어요 와

--- src/main/java/com/example/goldenage/product/controller/ProductController.java
+++ src/main/java/com/example/goldenage/product/controller/ProductController.java
@@ -1,12 +1,16 @@
 package com.example.goldenage.product.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * product 페이지의 기능 구현을 위한 클래스
  * @author HeeCHang
  * */
+@Controller
+@RequestMapping("/product")
 public class ProductController {
 
     /**
@@ -14,9 +18,9 @@ public class ProductController {
      *     product 페이지 조회 메소드
     * </pre>
     */
-    @GetMapping("/product")
+    @GetMapping("/productPage")
     public String productPage(){
 
-        return "product";
+        return "/product/productPage";
     }
 }
