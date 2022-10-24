8aad42a0d11fd522f6b824808457e9135ae9f242
intectme12 <intectme12@gmail.com>
2022-10-25 00:01:46 +0900

    jihwan main

--- src/main/java/com/goldenage/project/login/controller/Login.java
+++ src/main/java/com/goldenage/project/login/controller/Login.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin/**")
-public class login {
+@RequestMapping("/admin/*")
+public class Login {
 
     @GetMapping("/login")
     public String adminLogin(){
@@ -17,7 +17,7 @@ public class login {
     @GetMapping("/password")
     public String findPassword(){
 
-        return "/password";
+        return "/admin/password";
     }
 
     @GetMapping("/main")
