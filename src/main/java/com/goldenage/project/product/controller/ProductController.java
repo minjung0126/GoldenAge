a1414f43e85e18bf7cee71d5b1b13efef97760ec
Yu hee chang <yhc10215@gmail.com>
2022-10-28 07:21:20 +0000

    Hee chang

--- src/main/java/com/goldenage/project/product/controller/ProductController.java
+++ src/main/java/com/goldenage/project/product/controller/ProductController.java
@@ -2,7 +2,12 @@ package com.goldenage.project.product.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * product 페이지의 기능 구현을 위한 클래스
@@ -33,4 +38,24 @@ public class ProductController {
 
         return "/product/detailPage";
     }
+
+    @GetMapping("/adminDetailPage")
+    public String adminDetailPage(){
+
+        return "/product/adminDetailPage";
+    }
+    /**
+     * <pre>
+     *     싱글 파일 업로드를 위한 코드
+     * </pre>
+     * */
+//    @PostMapping("/upload/pic")
+//    public String singleFileUpload(@RequestParam String fileContent, @RequestParam MultipartFile singleFile, HttpServletRequest request){
+//
+//        // 전송받은 파일 및 파일 설명 값 가져오기
+//        System.out.println("singleFile : " + singleFile);
+//        System.out.println("fileContent : " + fileContent);
+//
+//    }
+
 }
