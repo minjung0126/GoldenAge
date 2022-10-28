dae6f3e076754d29af858ad01842d7ab96ca539e
minjeong <minjeong@jominjeong-ui-MacBookPro.local>
2022-10-28 16:20:17 +0900

    commit

--- src/main/java/com/goldenage/project/config/SpringSecurityConfig.java
+++ src/main/java/com/goldenage/project/config/SpringSecurityConfig.java
@@ -37,7 +37,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/product/**").hasRole("USER")
+//                .antMatchers("/product/**").hasRole("USER")
                 .antMatchers("/admin/login").hasRole("ADMIN")
                 .anyRequest().permitAll()
             .and()
