1d3b3fdc2ecb187fa272710b9739ad6d3954fa72
Yu hee chang <yhc10215@gmail.com>
2022-10-31 22:53:25 +0900

    최신화

--- src/main/java/com/goldenage/project/product/service/ProductServiceImpl.java
+++ src/main/java/com/goldenage/project/product/service/ProductServiceImpl.java
@@ -1,4 +1,23 @@
 package com.goldenage.project.product.service;
 
-public class ProductServiceImpl {
+import com.goldenage.project.product.model.dto.ProductDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+
+    public void productRegist(ProductDTO productDTO){
+
+    }
 }
