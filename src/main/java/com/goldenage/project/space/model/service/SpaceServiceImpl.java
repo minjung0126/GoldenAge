9a82e2a100d64b9828e4ce56c943baaa960e6000
Hyeri <hyeri5739@gmail.com>
2022-11-03 03:29:07 +0900

    리스트출력 삭제

--- src/main/java/com/goldenage/project/space/model/service/SpaceServiceImpl.java
+++ src/main/java/com/goldenage/project/space/model/service/SpaceServiceImpl.java
@@ -1,8 +1,14 @@
 package com.goldenage.project.space.model.service;
 
+import com.goldenage.project.exception.notice.NoticeDeleteException;
+import com.goldenage.project.exception.space.SpaceDeleteException;
 import com.goldenage.project.space.model.dao.SpaceMapper;
+import com.goldenage.project.space.model.dto.SpaceDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service("spaceService")
 public class SpaceServiceImpl implements SpaceService{
@@ -10,9 +16,33 @@ public class SpaceServiceImpl implements SpaceService{
     private SpaceMapper spaceMapper;
 
     @Autowired
-    public SpaceServiceImpl(SpaceMapper spaceMapper){
+    public SpaceServiceImpl(SpaceMapper spaceMapper) {
         this.spaceMapper = spaceMapper;
+
     }
 
+    // 관리자 연습실 리스트
+    @Override
+    public List<SpaceDTO> selectSpaceList() {
+
+        List<SpaceDTO> spaceList = spaceMapper.selectSpaceList();
+
+        return spaceList;
+    }
+
+    //연습실 삭제
+    @Override
+    @Transactional
+    public int deleteSpace(int spaceNum) throws SpaceDeleteException {
+        int result = spaceMapper.deleteSpaceFile(spaceNum);
+        int result2 = spaceMapper.deleteSpace(spaceNum);
+
+        if(!(result2 > 0)){
+
+            throw new SpaceDeleteException("연습실 삭제 실패!");
+        }
+
+        return result;
+    }
 }
 
