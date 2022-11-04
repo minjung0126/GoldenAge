0a1c4f77ba4376bed4665b6f25b51b4c7ca8ab17
intectme12 <intectme12@gmail.com>
2022-11-04 13:35:37 +0900

    jihwan

--- src/main/java/com/goldenage/project/product/controller/ProductController.java
+++ src/main/java/com/goldenage/project/product/controller/ProductController.java
@@ -69,6 +69,10 @@ public class ProductController {
         ProductDTO productDetail = productService.selectOneProduct(pd_num);
         List<ProductDetailDTO> productDetailDTOList = productService.selectAllProductDetail(pd_num);
 
+        for(ProductDetailDTO pd : productDetailDTOList){
+            System.out.println(pd);
+        }
+
         mv.addObject("productDetail", productDetail);
         mv.addObject("productDetailDTOList", productDetailDTOList);
 
@@ -278,6 +282,7 @@ public class ProductController {
 
     @PostMapping("/detailPage/update/poster")
     public String productDetailUpdatePoster(@ModelAttribute ProductDetailDTO productDetailDTO, @RequestParam(value = "detail_file_num", required = false) String detail_file_num, @RequestParam(value = "file", required = false) MultipartFile file, RedirectAttributes rttr) throws Exception{
+        log.info("pd_num : " + productDetailDTO.getPd_num());
 
         int pd_num = productDetailDTO.getPd_num();
 
@@ -325,7 +330,7 @@ public class ProductController {
         rttr.addFlashAttribute("message", "수정 성공!");
         rttr.addFlashAttribute("check", "success");
 
-        return "redirect:/product/product_update_poster?pd_num=" + pd_num;
+        return "redirect:/product/product_update_poster?detail_file_num=" + pd_num;
 
 
 
