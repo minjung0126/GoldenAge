6ddf0ee7f52bad4c13bba0ba608f5f9bd29e7ad1
Hyeri <hyeri5739@gmail.com>
2022-11-04 13:31:12 +0900

    마스터 머지를 위한 커밋

--- src/main/java/com/goldenage/project/space/model/dao/SpaceMapper.java
+++ src/main/java/com/goldenage/project/space/model/dao/SpaceMapper.java
@@ -1,15 +1,25 @@
 package com.goldenage.project.space.model.dao;
 
 import com.goldenage.project.space.model.dto.SpaceDTO;
+import com.goldenage.project.space.model.dto.SpacePhoDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SpaceMapper {
     List<SpaceDTO> selectSpaceList();
 
+    int insertSpace(SpaceDTO space);
+
     int deleteSpaceFile(int spaceNum);
 
     int deleteSpace(int spaceNum);
+
+    int insertSpacePhoto(Map<String, String> pho);
+
+    String selectNum();
+
+    void selectName();
 }
