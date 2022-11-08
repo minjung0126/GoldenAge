9e310b72a0c65844a48c79b0a87c944f0472be5a
intectme12 <intectme12@gmail.com>
2022-11-08 17:28:37 +0900

    jihwan

--- src/main/java/com/goldenage/project/main/controller/Main.java
+++ src/main/java/com/goldenage/project/main/controller/Main.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/*")
 public class Main {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
