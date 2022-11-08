db83bf9c50f2f76686b98292ce26fbdd1292171f
Hyeri <hyeri5739@gmail.com>
2022-11-08 20:09:08 +0900

    어딜

--- src/main/java/com/goldenage/project/main/controller/Main.java
+++ src/main/java/com/goldenage/project/main/controller/Main.java
@@ -9,12 +9,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
+@RequestMapping("/")
 public class Main {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
@@ -34,7 +36,7 @@ public class Main {
    //     return "/index";
    // }
 
-    @GetMapping({"mainPage"})
+    @GetMapping({"/","mainPage"})
     public ModelAndView mainProductPageList(HttpServletRequest request, ModelAndView mv){
         System.out.println("index : 인덱스왓니");
 
